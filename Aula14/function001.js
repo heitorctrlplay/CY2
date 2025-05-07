a0c3c3d670cdfc726ba4006c13836e0b72eb6616
Heitor <heitor.bertolani@escola.pr.gov.br>
2025-05-07 14:14:23 -0300

    Aula 14

---
+++ Aula14/function001.js
@@ -0,0 +1,11 @@
+function areaDoRetangulo(largura, altura) {
+    let area = largura*altura;
+    console.log(area);
+}
+areaDoRetangulo(5, 10)
+
+function areaDoTriangulo(base, altura){
+    let areaTriangulo = base*altura;
+    console.log(area);
+}
+areaDoTriangulo(5,10)
